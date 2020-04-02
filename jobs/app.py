f13d854ec8a57bb507310975106627160b623715
Christoffer Forsberg <christoffer.forsberg@infor.com>
2020-04-02 14:18:24 +0200

    minor changes

--- jobs/app.py
+++ jobs/app.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, "_connection", None)
     if connection == None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
-def exequte_sql(sql, values=(), commit=False, single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
-        result = connection.commit()
+        results = connection.commit()
     else:
-        result = cursor.fetchone() if single else cursor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
-    return result
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
