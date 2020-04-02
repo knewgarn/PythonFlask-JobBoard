e9b158e2f26c65f43d0d0de72760deb10aef405a
Christoffer Forsberg <christoffer.forsberg@infor.com>
2020-04-02 14:13:39 +0200

    changed _connection

--- jobs/app.py
+++ jobs/app.py
@@ -5,7 +5,7 @@ PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 def open_connection():
-    connection = getattr(g, '_connection_', None)
+    connection = getattr(g, "_connection", None)
     if connection == None:
         connection, g._connection_ = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
@@ -23,7 +23,7 @@ def exequte_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g, "_connection_", None)
+    connection = getattr(g, "_connection", None)
     if connection is not None:
         connection.close()
 
